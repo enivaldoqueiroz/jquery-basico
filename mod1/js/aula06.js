bbe956e8b0301875407bd049dfbaad35416b70b0
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-01-29 19:13:04 -0300

    Evendo de um click e evento de duplo click

--- mod1/js/aula06.js
+++ mod1/js/aula06.js
@@ -1,9 +1,16 @@
 $(function(){
     //Passando o valor do classe ex1 para a variavel ex
     var ex = $('.ex1');
+
+    //Evento de mouse com um click
     $('.ev1').click(function(){
         $(this).css("background","#ccc");
         ex.text("Você clicou!");
     });
-    
+
+    //Evento de mouse com um duplo click
+    $('.ev2').dblclick(function(){
+        $(this).css("background","#ccc");
+        ex.text("Você deu dois cliques!");
+    });
 });
