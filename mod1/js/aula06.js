66843e59489126a4ac2eacab5083582a4436d1be
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-01-29 19:01:53 -0300

    Implementação do clique no jquery

--- mod1/js/aula06.js
+++ mod1/js/aula06.js
@@ -1,5 +1,9 @@
 $(function(){
-
+    //Passando o valor do classe ex1 para a variavel ex
+    var ex = $('.ex1');
+    $('.ev1').click(function(){
+        $(this).css("background","#ccc");
+        ex.text("Você clicou!");
+    });
     
-
 });
