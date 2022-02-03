8f2d4d487ea02848ec6fc405e71df5f1ea9e62e3
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-02-02 23:23:14 -0300

    Trabalhando com focus. blur e change

--- mod1/js/aula08.js
+++ mod1/js/aula08.js
@@ -20,6 +20,21 @@ $(function(){
             });
     });
 
-    
+    var ex = $('.ex1');
+    /* $('.ev1').focus(function(){
+        ex.text($(this).attr('title'));
+    }).keyup(function(){
+        if($(this).val() == 'pontocom'){
+            $('.ev2').focus();
+        }
+    }); */
+
+    /* $('.ev1').blur(function(){
+        ex.text("Saida do campo: " + $(this).attr('name'));
+    }); */
+
+    $('.ev1').change(function(){
+        ex.text("Campo alterado: " + $('this').val());
+    });
 
 });
